8e1fd95f0e5398eb2a08b568804b6bff14bea460
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2021-11-29 10:54:03 +0530

    Update ktorm_Notes.kt

--- ktorm_Notes.kt
+++ ktorm_Notes.kt
@@ -9,3 +9,15 @@ Add Dependency
   For MySQL : 
             https://mvnrepository.com/artifact/mysql/mysql-connector-java
             implementation group: 'mysql', name: 'mysql-connector-java', version: '8.0.27'
+
+
+Connect to Database
+//Make sure you create DB in phpmyadmin and run project and if Project connect to DB then you will see following info in Console
+val database = Database.connect(
+        url = "jdbc:mysql://localhost/test_ktorm",
+        driver = "com.mysql.cj.jdbc.Driver",
+        user = "root",
+        password = "")
+    //org.ktorm.database - Connected to jdbc:mysql://localhost/test_ktorm, productName: MySQL, productVersion: 5.5.5-10.4.21-MariaDB, logger: org.ktorm.logging.Slf4jLoggerAdapter@6bc28a83, dialect: org.ktorm.database.SqlDialectKt$detectDialectImplementation$1@324c64cd
+
+
