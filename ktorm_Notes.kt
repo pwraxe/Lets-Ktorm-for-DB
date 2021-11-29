9bc518c1378b97571f741a2b2489b9bc5eb163bb
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2021-11-29 10:36:56 +0530

    Update ktorm_Notes.kt

--- ktorm_Notes.kt
+++ ktorm_Notes.kt
@@ -1,1 +1,11 @@
-- k-t-o-r f-o-r d-b
+Official Website : https://www.ktorm.org/
+
+Add Dependency  
+      
+  For Ktorm 
+            https://mvnrepository.com/artifact/org.ktorm/ktorm-core 
+            implementation group: 'org.ktorm', name: 'ktorm-core', version: '3.4.1'
+
+  For MySQL : 
+            https://mvnrepository.com/artifact/mysql/mysql-connector-java
+            implementation group: 'mysql', name: 'mysql-connector-java', version: '8.0.27'
